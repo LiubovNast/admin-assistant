c554cd271eb71f1d99cd75a67d02e11ed5822825
LiubovNast <77754627+LiubovNast@users.noreply.github.com>
2021-11-14 10:33:13 +0200

    init commit

---
+++ src/main/java/com/alevel/nix7/adminassistant/AdminAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.alevel.nix7.adminassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alevel/nix7/adminassistant/AdminAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alevel.nix7.adminassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
